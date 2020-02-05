e75aedc568bb123eff9eef27a8eacd413b77e15b
Stefan Zdravkov <szdravkov93@gmail.com>
2020-02-06 00:57:09 +0100

    Update index.js

--- hw2/index.js
+++ hw2/index.js
@@ -12,19 +12,20 @@ const read = ( file ) =>{
 
 read('lorem.txt')
 .then(data =>{
-    let sentencesNo = data.split('.').length; // fix  this
-    let wordsNo = data.split(' ').length;
-    let charNo = data.length - data.split(' ').length;
+    let sentencesNo = data.split('.').length; // number of sentences - fix  this
+    let wordsNo = data.split(' ').length; // number of words
+    let charNo = data.length - data.split(' ').length;// number of characters
    console.log(`this text has ${sentencesNo} sentences, ${wordsNo} words and ${charNo} characters`);
-   let words = data.split(' ');
+   let words = data.split(' '); // to split words into an array
 //counting length  of words
-   let wordsBigger = 0;
-   let wordsSmaller = 0;
-   let wordsEven = 0;
+   let wordsBigger = 0; // word > 7
+   let wordsSmaller = 0; // word < 7
+   let wordsEven = 0; // word == 7
     for (let i=0;i<wordsNo;i++) {
         //if(words[i].includes('.') || words[i].includes(',') || words[i].includes('!') || words[i].includes('?') || words[i].includes('...')){ // the regex solution comes in handy :)
         let pWord = words[i].replace(/[^0-9a-zA-Z]+/g, ""); // regex to remove everything from a word except characters a-z, A-Z and 0-9
         //}
+        // logic for counting words length.. check above
         if (pWord.length > 7) {
             wordsBigger++
         } else if (pWord.length < 7) {
@@ -32,10 +33,41 @@ read('lorem.txt')
         } else if (pWord.length == 7) {
             wordsEven++
         }
+        // end of logic
     }
     console.log(`${wordsBigger} of these words has length bigger then "7", ${wordsSmaller} less then "7" and ${wordsEven} are even with "7"`);
+    
+    // cela funcija ludilo
+    const findDuplicates = (arr) => {
+        let sorted_arr = arr.slice().sort();
+        let results = [];
+        //////////down
+        var current = null;
+        var cnt = 0;
+        //////up
 
+        for (let i = 0; i < sorted_arr.length - 1; i++) {
+           /* if (sorted_arr[i + 1] == sorted_arr[i]) {
+              results.push(sorted_arr[i]);
+            }*/
+            if (sorted_arr[i].replace(/[^0-9a-zA-Z]+/g, "") != current){
+                if(cnt>0){
+                    console.log(`${current}  --> ${cnt} times`)
+                }
+                current = sorted_arr[i].replace(/[^0-9a-zA-Z]+/g, "");;
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+          }
 
+          if(cnt >0){
+            console.log(`${current} comes --> ${cnt} times`)
+          }
+          
+          return results;
+    }
+    console.log(findDuplicates(words))
 })
 .catch(err => {
     console.log('an error has occured while writing to file');
