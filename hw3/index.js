7aa4286cc595cc5c5b2d94c3777c35624197dc9e
Stefan Zdravkov <szdravkov93@gmail.com>
2020-02-09 18:15:14 +0100

    in com

--- hw3/index.js
+++ hw3/index.js
@@ -1,9 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+const fs = require('fs');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
+
+
+/*
+const read = ( file ) =>{
+   var rawFile = new XMLHttpRequest();
+   rawFile.open("GET", file, false);
+   rawFile.onreadystatechange = () => {
+       if(rawFile.readyState === 4){
+           if(rawFile.status === 200 || rawFile.status == 0){
+               var allText = rawFile.responseText;
+               console.log(allText)
+           }
+       }
+   }
+   rawFile.send(null)
+}*/
+
+const read = ( file ) => {
+    return new Promise(( success, fail ) => {
+        fs.readFile(file, 'utf8', (err, data) =>{
+            if(err) 
+                return fail(err);
+            return success(data);
+        })
+    })
+}
+
+const showText = (data) =>{
+    let ime = data.split(' ')
+
+    console.log(ime)
+}
+
+
+app.get('/', (req, res) =>{
+    res.send('ok');
+})
+
+app.get('/imenik', (req, res) =>{
+    res.send(read('imenik.txt')
+    .then(data => data)
+    //.then(text => console.log(text))
+    .catch(err => { console.log('an error has occured while writing to file'); })
+    );
+})
+
+//read('imenik.txt').then((result)=>{console.log(result)})
 
 
 
