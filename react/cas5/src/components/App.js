5e2045aa3cb1920b251d7e45b443e61afc195271
Stefan Zdravkov <szdravkov93@gmail.com>
2020-03-30 17:20:57 +0200

    init commit

---
+++ react/cas5/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <h2>Hello</h2>
+      </div>
+    );
+  }
+}
